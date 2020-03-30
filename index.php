3930c2abee6a8730d09048654f18d2b08ffde963
hothson <hothson@gmail.com>
2020-03-30 13:56:38 +0700

    gif file

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="video element-with-video-bg jquery-background-video-wrapper">
-                    <video class="my-background-video jquery-background-video" data-bgvideo="true" autoplay loop muted playsinline>
+                    <video class="my-background-video jquery-background-video" autoplay loop muted playsinline>
                         <source id="video" src="videos/World - 1992.mp4" type="video/mp4">
                     </video>
                     <img src="images/logo_plus_slogan.png" alt="">
@@ -474,18 +474,10 @@
     <script src="js/jquery.background-video.js"></script>
 
     <script>
-        $('.my-background-video').bgVideo({
-            fullScreen: false, // Sets the video to be fixed to the full window - your <video> and it's container should be direct descendents of the <body> tag
-            fadeIn: 86400, // Milliseconds to fade video in/out (0 for no fade)
-            pauseAfter: 86400, // Seconds to play before pausing (0 for forever)
-            fadeOnPause: false, // For all (including manual) pauses
-            fadeOnEnd: true, // When we've reached the pauseAfter time
-            showPausePlay: true, // Show pause/play button
-            pausePlayXPos: 'right', // left|right|center
-            pausePlayYPos: 'top', // top|bottom|center
-            pausePlayXOffset: '15px', // pixels or percent from side - ignored if positioned center
-            pausePlayYOffset: '15px' // pixels or percent from top/bottom - ignored if positioned center
-        });
+       $(document).ready(function() {
+            // $('.my-background-video').bgVideo();
+       });
+
         $.fn.bgVideo.defaults.fadeIn = 0;
         $.fn.bgVideo.defaults.pauseAfter = 0;
 
